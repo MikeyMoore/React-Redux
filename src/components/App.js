949a49ece9ead12ffcefcaf24492ca8c8619bffe
MikeyMoore <icarusjedi@gmail.com>
2017-04-12 00:23:53 -0700

    shopping cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
 import Items from '../containers/Items';
+import ShoppingCart from '../containers/ShoppingCart';
 
 export default class App extends Component {
 
@@ -8,6 +9,7 @@ export default class App extends Component {
     return (
       <div className="App-mainbody">
         <Items />
+        <ShoppingCart />
       </div>
     )
   }
---
+++ src/containers/ShoppingCart/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './ShoppingCart.css';
+
+export class ShoppingCart extends Component {
+
+  renderShoppingCart = () => {
+    const { shoppingCart } = this.props;
+    return shoppingCart.map((item) => {
+      return (
+        <li
+          onClick={() => console.log('remove from cart')}
+          key={item.id}
+          className={'SC-list-item'}
+        >
+          <img role="presentation" src={item.link} />
+          {item.title}
+        </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <ul className={'SC-list'}>
+        {this.renderShoppingCart()}
+      </ul>
+    )
+  }
+
+}
+
+const mapStateToProps = ({ shoppingCart }) => ({
+  shoppingCart,
+})
+
+/*
+  function connect(mapStateToProps, mapDispatchToProps) {
+    return function(ourComponent) {
+      //it passes in state, as well as actions, via props
+    }
+  }
+*/
+
+export default connect(mapStateToProps)(ShoppingCart)
